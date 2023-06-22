45a02b9de5147e67d4b32d5b23fffe45fea9c46b
vinicius ochner <vinicius.ochner@fulltechpro.com.br>
2023-06-21 23:17:54 -0300

    rsa algoritmo

---
+++ Decripta/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Numerics;
+using System.Text;
+
+namespace RSAEncryption
+{
+    class Decrypt
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 3)
+            {
+                Console.WriteLine("Modo de uso: <key_file> <input_file> <output_file>");
+                return;
+            }
+
+            string keyFile = args[0];
+            string inputFile = args[1];
+            string outputFile = args[2];
+
+            string[] keyLines = File.ReadAllLines(keyFile);
+            var n = BigInteger.Parse(keyLines[0]);
+            var d = BigInteger.Parse(keyLines[1]);
+
+            string[] encodedChunks = File.ReadAllLines(inputFile);
+            
+            string plaintext = RSADecrypt(encodedChunks, d, n);
+
+            File.WriteAllText(outputFile, plaintext);
+        }
+
+        static string RSADecrypt(string[] encodedChunks, BigInteger d, BigInteger n)
+        {
+            var decryptedData = new StringBuilder();
+            
+            foreach (string encodedChunk in encodedChunks)
+            {
+                var chunk = BigInteger.ModPow(BigInteger.Parse(encodedChunk), d, n);
+                var decryptedChunk = Encoding.ASCII.GetString(chunk.ToByteArray());
+                decryptedData.Append(decryptedChunk);
+            }
+
+            return decryptedData.ToString();
+        }
+    }
+}
---
+++ Encripta/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Numerics;
+using System.Text;
+
+namespace RSAEncryption
+{
+    class Encrypt
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 3)
+            {
+                Console.WriteLine("Modo de uso: <key_file> <input_file> <output_file>");
+                return;
+            }
+
+            string keyFile = args[0];
+            string inputFile = args[1];
+            string outputFile = args[2];
+
+            string[] keyLines = File.ReadAllLines(keyFile);
+            BigInteger n = BigInteger.Parse(keyLines[0]);
+            BigInteger e = BigInteger.Parse(keyLines[1]);
+
+            string plaintext = File.ReadAllText(inputFile);
+
+            byte[] plaintextBytes = Encoding.UTF8.GetBytes(plaintext);
+            string base64Text = Convert.ToBase64String(plaintextBytes);
+
+            byte[] base64Bytes = Encoding.UTF8.GetBytes(base64Text);
+            BigInteger chunkSize = CalculateChunkSize(n);
+            BigInteger encodedChunk;
+            StringBuilder encodedChunksBuilder = new StringBuilder();
+            for (int i = 0; i < base64Bytes.Length; i += (int)chunkSize)
+            {
+                byte[] chunk = new byte[Math.Min((int)chunkSize, base64Bytes.Length - i)];
+                Array.Copy(base64Bytes, i, chunk, 0, chunk.Length);
+                encodedChunk = BigInteger.ModPow(new BigInteger(chunk), e, n);
+                encodedChunksBuilder.AppendLine(encodedChunk.ToString());
+            }
+
+            // Writing encrypted data to output file
+            File.WriteAllText(outputFile, encodedChunksBuilder.ToString());
+        }
+
+        static BigInteger CalculateChunkSize(BigInteger n)
+        {
+            int bitLength = n.ToByteArray().Length * 8;
+            int byteLength = (bitLength + 7) / 8;
+            int maxChunkSize = byteLength - 1;
+            return new BigInteger(maxChunkSize);
+        }
+    }
+}
